9edbee4c0cb7e1b5fa8ba47833bb8948cfbd6536
Javier Perales <fj.perales@edu.gva.es>
2025-11-24 10:05:20 +0100

    Update CatvsDog.py

--- st-app/CatvsDog.py
+++ st-app/CatvsDog.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
 
+# Configuración de la página
+st.set_page_config(page_title="Reconocimiento Perros vs Gatos", page_icon="🐾")
+
+st.title("🐶 Detector de Mascotas 🐱")
+st.write("Usa la cámara para saber si es un perro o un gato.")
